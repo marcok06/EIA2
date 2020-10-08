7caad42002e720156c6eb4edcb0f7b29b961e4cc
marcok06 <48826814+marcok06@users.noreply.github.com>
2020-10-08 19:31:35 +0200

    Uebung00

---
+++ Uebung00/Uebung00.js
@@ -0,0 +1,7 @@
+"use strict";
+var person = prompt("Wie heißt du?", "Dein Name");
+if (person != null) {
+    document.getElementById("Name").innerHTML =
+        "What's squeezin " + person + "?";
+}
+//# sourceMappingURL=Uebung00.js.map
---
+++ Uebung00/Uebung00.ts
@@ -0,0 +1,6 @@
+var person = prompt("Wie heißt du?", "Larry");
+
+if (person != null) {
+  document.getElementById("Name").innerHTML =
+  "Hey whats squeezin + person + "?"
+} 
