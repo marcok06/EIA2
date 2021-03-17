95c483af28566e1c3ea89290f33c40a2064799ef
marcok06 <48826814+marcok06@users.noreply.github.com>
2021-03-17 19:44:43 +0100

    SoSe21

--- Uebung00/Uebung00.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var person = prompt("Wie heißt du?", "Dein Name");
-if (person != null) {
-    document.getElementById("Name").innerHTML =
-        "What's squeezin " + person + "?";
-}
-//# sourceMappingURL=Uebung00.js.map
--- Uebung00/Uebung00.ts
+++
@@ -1,6 +0,0 @@
-var person = prompt("Wie heißt du?", "Larry");
-
-if (person != null) {
-  document.getElementById("Name").innerHTML =
-  "Hey whats squeezin + person + "?"
-} 
